00cc595fdf5b4e5f7c253bd894322a823e45e821
Hieu Tran <hieutran.fu@gmail.com>
2019-01-30 00:02:45 +0700

    feat(react-silk): heading components

--- packages/react-silk-docs/src/pages/index.tsx
+++ packages/react-silk-docs/src/pages/index.tsx
@@ -3,13 +3,28 @@ import React from 'react'
 import Layout from '../components/layout'
 import SEO from '../components/seo'
 
-import { Heading } from 'react-silk'
+import {
+  Heading1,
+  Heading2,
+  Heading3,
+  Heading4,
+  Heading5,
+  Heading6,
+  Text } from 'react-silk'
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <Heading>Hi people</Heading>
-    <p>Welcome to React Silk site.</p>
+    <div style={{ display: 'flex', flexDirection: 'column' }}>
+      <Heading1>This is heading 1</Heading1>
+      <Heading2>This is heading 1</Heading2>
+      <Heading3>This is heading 1</Heading3>
+      <Heading4>This is heading 1</Heading4>
+      <Heading5>This is heading 1</Heading5>
+      <Heading6>This is heading 1</Heading6>
+      <Text>This is normal text</Text>
+      
+    </div>
   </Layout>
 )
 
