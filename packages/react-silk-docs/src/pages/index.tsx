9a7d3b185c0fc8ef01b7e46f508716be2f109443
Hieu Tran <hieutran.fu@gmail.com>
2019-01-29 23:32:32 +0700

    Clean up deps

--- packages/react-silk-docs/src/pages/index.tsx
+++ packages/react-silk-docs/src/pages/index.tsx
@@ -1,8 +1,6 @@
 import React from 'react'
-import { Link } from 'gatsby'
 
 import Layout from '../components/layout'
-import Image from '../components/image'
 import SEO from '../components/seo'
 
 import { Heading } from 'react-silk'
@@ -12,10 +10,6 @@ const IndexPage = () => (
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
     <Heading>Hi people</Heading>
     <p>Welcome to React Silk site.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
 
