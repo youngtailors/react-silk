16fcaa38af90277912068e9ef4c89f6f4470b62e
Evan N <dacsang97@gmail.com>
2019-01-30 15:09:12 +0700

    (react-silk-docs) add react-art

--- packages/react-silk-docs/src/pages/index.tsx
+++ packages/react-silk-docs/src/pages/index.tsx
@@ -10,20 +10,20 @@ import {
   Heading4,
   Heading5,
   Heading6,
-  Text } from 'react-silk'
+  Text,
+} from 'react-silk'
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
     <div style={{ display: 'flex', flexDirection: 'column' }}>
       <Heading1>This is heading 1</Heading1>
-      <Heading2>This is heading 1</Heading2>
-      <Heading3>This is heading 1</Heading3>
-      <Heading4>This is heading 1</Heading4>
-      <Heading5>This is heading 1</Heading5>
-      <Heading6>This is heading 1</Heading6>
+      <Heading2>This is heading 2</Heading2>
+      <Heading3>This is heading 3</Heading3>
+      <Heading4>This is heading 4</Heading4>
+      <Heading5>This is heading 5</Heading5>
+      <Heading6>This is heading 6</Heading6>
       <Text>This is normal text</Text>
-      
     </div>
   </Layout>
 )
