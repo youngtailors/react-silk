4450e81e632fdfb45815cae40a0297ebe306dbcf
Evan N <dacsang97@gmail.com>
2019-02-01 13:36:05 +0700

    chore(docs) remove unused code

--- packages/react-silk-docs/src/pages/index.tsx
+++ packages/react-silk-docs/src/pages/index.tsx
@@ -2,17 +2,6 @@ import React from 'react'
 
 import Layout from '../components/Layout'
 import SEO from '../components/seo'
-
-import {
-  Heading1,
-  Heading2,
-  Heading3,
-  Heading4,
-  Heading5,
-  Heading6,
-  Text,
-  Button,
-} from 'react-silk-ui'
 import { Banner } from '../components/Banner'
 
 const IndexPage = () => (
