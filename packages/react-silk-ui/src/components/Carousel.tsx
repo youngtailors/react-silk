d5cf60fcd0d1e156aa50fa4211bc00cc27792d7b
Mark Allen <doantufptu@gmail.com>
2019-02-09 13:56:38 +0700

    [slide] image without animations

--- packages/react-silk-ui/src/components/Carousel.tsx
+++ packages/react-silk-ui/src/components/Carousel.tsx
@@ -1,11 +1,46 @@
 import * as React from 'react'
-import { View, StyleSheet, Image, Text } from 'react-native'
+import { View, StyleSheet, Image, TouchableOpacity } from 'react-native'
+import Colors from './Colors'
+import { Icon } from './Icon'
 
 const styles = StyleSheet.create({
   container: {},
 
-  show: {
+  hide: {
     display: 'none',
+  },
+
+  show: {
+    display: 'flex',
+  },
+
+  iconLeft: {
+    position: 'absolute',
+    width: '4%',
+    justifyContent: 'center',
+    alignContent: 'center',
+    top: 0,
+    bottom: 0,
+    left: 10,
+  },
+
+  iconRight: {
+    position: 'absolute',
+    justifyContent: 'center',
+    alignContent: 'center',
+    width: '4%',
+    top: 0,
+    bottom: 0,
+    right: 10,
+  },
+
+  icon: {
+    display: 'flex',
+    justifyContent: 'center',
+  },
+
+  slideWrapper: {
+    position: 'relative',
   },
 })
 
@@ -13,48 +48,85 @@ export interface Props {
   autoSlide: boolean
   interval: number
   dots: boolean
-  data?: any
+  data: any
   arrow: boolean
-  color: string
-  style: any
 }
 
 interface State {
-  data: any
+  index: number
 }
-
-export const Carousel = ({
-  autoSlide,
-  interval,
-  dots,
-  data,
-  arrow,
-  color,
-  style,
-}: Props) => {
-  console.log(data)
-  if (data.length < 0) {
-    console.log('cac')
-    return null
+export class Carousel extends React.Component<Props, State> {
+  constructor(props: Props) {
+    super(props)
+    this.state = {
+      index: 0,
+    }
   }
-  return (
-    <View style={styles.container}>
-      {data.map((item: any, index: any) => (
-        <View key={index}>
-          <Image
-            style={{ width: '100%', height: 400 }}
-            source={{ uri: `${item}` }}
-          />
-        </View>
-      ))}
-    </View>
-  )
-}
 
-Carousel.defaultProps = {
-  autoSlide: false,
-  interval: 500,
-  dots: true,
-  arrow: true,
-  color: '#cecece',
+  handleSlide = (isRight: boolean) => {
+    const { index } = this.state
+    let currentIndex = index
+    const { data } = this.props
+    if (isRight) {
+      if (currentIndex < data.length - 1) {
+        currentIndex++
+      } else {
+        currentIndex = 0
+      }
+    } else {
+      if (currentIndex === 0) {
+        currentIndex = data.length - 1
+      } else {
+        currentIndex--
+      }
+    }
+    this.setState({ index: currentIndex })
+  }
+
+  render() {
+    const { data } = this.props
+    const { index } = this.state
+    return (
+      <View style={styles.container}>
+        {data.map((item: any, i: number) => (
+          <View key={i} style={i !== index ? styles.hide : styles.show}>
+            <View style={styles.slideWrapper}>
+              <Image
+                style={{ width: '100%', height: 400 }}
+                source={{ uri: item }}
+              />
+              <View style={styles.iconLeft}>
+                <TouchableOpacity
+                  onPress={() => {
+                    this.handleSlide(false)
+                  }}
+                >
+                  <Icon
+                    style={styles.icon}
+                    name="angle-left"
+                    color={Colors.white}
+                    size={46}
+                  />
+                </TouchableOpacity>
+              </View>
+              <View style={styles.iconRight}>
+                <TouchableOpacity
+                  onPress={() => {
+                    this.handleSlide(true)
+                  }}
+                >
+                  <Icon
+                    style={styles.icon}
+                    name="angle-right"
+                    color={Colors.white}
+                    size={46}
+                  />
+                </TouchableOpacity>
+              </View>
+            </View>
+          </View>
+        ))}
+      </View>
+    )
+  }
 }
