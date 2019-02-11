807854626d215b30f34610574b7e61b9bd731b5e
Mark Allen <doantufptu@gmail.com>
2019-02-11 20:37:39 +0700

    [wip] animation

--- packages/react-silk-ui/src/components/Carousel.tsx
+++ packages/react-silk-ui/src/components/Carousel.tsx
@@ -1,18 +1,13 @@
 import React from 'react'
 import { View, StyleSheet, Image, TouchableOpacity, Text } from 'react-native'
-import { Transition } from 'react-spring/renderprops'
+import { Transition, animated } from 'react-spring/renderprops'
 import Colors from './Colors'
 import { Icon } from './Icon'
 
 const styles = StyleSheet.create({
-  container: {},
-
-  hide: {
-    display: 'none',
-  },
-
-  show: {
-    display: 'flex',
+  container: {
+    overflow: 'hidden',
+    flexDirection: 'row',
   },
 
   iconLeft: {
@@ -41,6 +36,10 @@ const styles = StyleSheet.create({
   },
 
   slideWrapper: {
+    width: '100%',
+  },
+
+  slide: {
     position: 'relative',
   },
 
@@ -48,14 +47,28 @@ const styles = StyleSheet.create({
     width: '100%',
     height: 400,
   },
+
+  delimiters: {
+    position: 'absolute',
+    width: '100%',
+    bottom: 10,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  dots: {
+    paddingHorizontal: 4,
+  },
 })
 
 export interface Props {
-  autoSlide: boolean
-  interval: number
-  dots: boolean
+  autoSlide?: boolean
+  interval?: number
+  duration?: number
+  dots?: boolean
   data: any
-  arrow: boolean
+  arrow?: boolean
 }
 
 export class Carousel extends React.Component<Props> {
@@ -91,50 +104,75 @@ export class Carousel extends React.Component<Props> {
     const { data } = this.props
     return (
       <View style={styles.container}>
-        <Transition
-          items={data[index]}
-          from={{ transform: 'translate3d(100%,0,0)' }}
-          enter={{ transform: 'translate3d(0%,0,0)' }}
-          leave={{ transform: 'translate3d(-100%,0,0)' }}
-        >
-          {item => (props: any) => (
-            <View style={props}>
-              <View>
-                <View style={styles.slideWrapper}>
-                  <Image style={styles.slideStyle} source={{ uri: item }} />
-                  <View style={styles.iconLeft}>
-                    <TouchableOpacity
-                      onPress={() => {
-                        this.handleSlide(false)
-                      }}
-                    >
-                      <Icon
-                        style={styles.icon}
-                        name="angle-left"
-                        color={Colors.white}
-                        size={46}
-                      />
-                    </TouchableOpacity>
-                  </View>
-                  <View style={styles.iconRight}>
-                    <TouchableOpacity
-                      onPress={() => {
-                        this.handleSlide(true)
-                      }}
-                    >
-                      <Icon
-                        style={styles.icon}
-                        name="angle-right"
-                        color={Colors.white}
-                        size={46}
-                      />
-                    </TouchableOpacity>
-                  </View>
-                </View>
+        {data.map((item: any, i: number) => {
+          const from: string = `translate3d(100% ,0 ,0)`
+          const enter: string = 'translate3d(0% ,0 ,0)'
+          const leave: string = `translate3d(-100% ,0 ,0)`
+          return (
+            i === index && (
+              <View key={`${i}_slide`} style={styles.slideWrapper}>
+                <Transition
+                  items={item}
+                  from={{ transform: from }}
+                  enter={{ transform: enter }}
+                  leave={{ transform: leave }}
+                >
+                  {item => (props: any) => (
+                    <animated.div style={props}>
+                      <View>
+                        <View style={styles.slide}>
+                          <Image
+                            style={styles.slideStyle}
+                            source={{ uri: item }}
+                          />
+                          <View style={styles.iconLeft}>
+                            <TouchableOpacity
+                              onPress={() => {
+                                this.handleSlide(false)
+                              }}
+                            >
+                              <Icon
+                                style={styles.icon}
+                                name="angle-left"
+                                color={Colors.white}
+                                size={46}
+                              />
+                            </TouchableOpacity>
+                          </View>
+                          <View style={styles.iconRight}>
+                            <TouchableOpacity
+                              onPress={() => {
+                                this.handleSlide(true)
+                              }}
+                            >
+                              <Icon
+                                style={styles.icon}
+                                name="angle-right"
+                                color={Colors.white}
+                                size={46}
+                              />
+                            </TouchableOpacity>
+                          </View>
+                          <View style={styles.delimiters}>
+                            {data.map((_: any, id: number) => (
+                              <Icon
+                                key={`${id}_dot`}
+                                style={styles.dots}
+                                name="circle-o"
+                                color={Colors.white}
+                                size={id === index ? 14 : 9}
+                              />
+                            ))}
+                          </View>
+                        </View>
+                      </View>
+                    </animated.div>
+                  )}
+                </Transition>
               </View>
-            </View>
-          )}
-        </Transition>
+            )
+          )
+        })}
       </View>
     )
   }
